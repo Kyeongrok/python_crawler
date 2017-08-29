207dd6b6489cc9acd455f0c3880077322a639a0a
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-08-29 15:58:37 +0900

    first commit

---
+++ bs4_hello.py
@@ -0,0 +1,6 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://pythonscraping.com/pages/page1.html")
+bsObj = BeautifulSoup(html.read(), "html.parser")
+print(bsObj.h1)
