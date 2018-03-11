c456aa070bbab0f9730c6ec000f382fd5e9bb8e5
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-03-11 11:51:08 +0900

    add window driver

--- naver/__init__.py
+++ naver/__init__.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome('../chromedriver')
+driver = webdriver.Chrome('../chrome/mac/chromedriver')
 driver.implicitly_wait(3)
 
 driver.get('https://www.naver.com/')
 
-driver.find_element_by_name('id').send_keys('oceanfog')
+driver.find_element_by_name('id').send_keys('oceanfog2')
 driver.find_element_by_name('pw').send_keys('') # 사용할 패스워드
 
 driver.find_element_by_css_selector('.login .btn_login input').submit()
