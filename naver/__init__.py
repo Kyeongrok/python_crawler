aa6abda74fb6a3aa530660b1bef4bad590e70bc9
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-03-11 12:08:46 +0900

    대기 시간 추가

--- naver/__init__.py
+++ naver/__init__.py
@@ -7,10 +7,11 @@ driver.implicitly_wait(3)
 driver.get('https://www.naver.com/')
 
 driver.find_element_by_name('id').send_keys('oceanfog')
-driver.find_element_by_name('pw').send_keys('') # 사용할 패스워드
+driver.find_element_by_name('pw').send_keys('$Rhat1249') # 사용할 패스워드
 
 driver.find_element_by_css_selector('.login .btn_login input').submit()
 
+driver.implicitly_wait(5)
 driver.get("https://nid.naver.com/user2/api/route.nhn?m=routePcMyInfo")
 
 html = driver.page_source
