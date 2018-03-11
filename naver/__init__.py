bc2e1c334095c0f311ee86661ec04346dc841a5e
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-03-11 12:03:47 +0900

    내정보로 이동해서 이름 나오게 하기

--- naver/__init__.py
+++ naver/__init__.py
@@ -6,15 +6,21 @@ driver.implicitly_wait(3)
 
 driver.get('https://www.naver.com/')
 
-driver.find_element_by_name('id').send_keys('oceanfog2')
+driver.find_element_by_name('id').send_keys('oceanfog')
 driver.find_element_by_name('pw').send_keys('') # 사용할 패스워드
 
 driver.find_element_by_css_selector('.login .btn_login input').submit()
 
+driver.get("https://nid.naver.com/user2/api/route.nhn?m=routePcMyInfo")
+
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-encodedHtml = html.encode('utf-8')
-print(encodedHtml)
+print(html)
+
+
+name = soup.find("span", {"class":"gnb_name"})
+
+print(name)
 
 driver.close()
