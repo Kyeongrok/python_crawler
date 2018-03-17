6235bdbe1affc32cf5fd74b57f1a1b50eb6bd274
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-03-17 15:19:37 +0900

    allOrdersParser2

--- ebay/allOrdersParser.py
+++ ebay/allOrdersParser.py
@@ -1,9 +1,37 @@
+from bs4 import BeautifulSoup
+
 fileName = "./allorders.html"
 def printLineByLine(file_path):
     with open(file_path, 'rt', encoding='utf-8') as f:
-        line = f.readline()
+        line = f.read()
         return line
 
 
 result = printLineByLine(fileName)
-print(result)
+# print(result)
+bsObj = BeautifulSoup(result, "html.parser")
+content = bsObj.find("table", {"id":"content"})
+mu_active_tbl_id = content.find("div", {"id":"mu_active_tbl_id"})
+tbodys = mu_active_tbl_id.findAll("tbody")
+
+for item in tbodys:
+    try:
+        buyerEmail = item.find("td", {"id":"BuyerEmail"})
+        purchasedQty = item.find("td", {"id": "PurchasedQty"})
+        qty = purchasedQty.find("div").text
+
+        aTag = buyerEmail.find("a")
+        try:
+            productName = aTag['title']
+            print(productName)
+        except KeyError:
+            print(qty)
+            pass
+
+    except AttributeError:
+        pass
+
+
+
+
+
--- ebay/ebaySellingParser.py
+++ ebay/ebaySellingParser.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 # 문자열에서 필요한 것만 뽑아내는 것
 
 # 원하는 텍스트를 뽑아오는 방법
-fileName = "./hello.txt"
+fileName = "./hello.html"
 def printLineByLine(file_path):
     with open(file_path, 'rt', encoding='utf-8') as f:
         line = f.readline()
@@ -11,10 +11,11 @@ def printLineByLine(file_path):
 
 
 result = printLineByLine(fileName)
-soup = BeautifulSoup(result, 'html.parser')
-summary_bar_columns = soup.find("div",{"class":"summary-bar"}).findAll("div", {"class":"summary-bar-column"})
-
-print(summary_bar_columns)
-
-for item in summary_bar_columns:
-    print(item)
+print(result)
+# soup = BeautifulSoup(result, 'html.parser')
+# summary_bar_columns = soup.find("div",{"class":"summary-bar"}).findAll("div", {"class":"summary-bar-column"})
+#
+# print(summary_bar_columns)
+#
+# for item in summary_bar_columns:
+#     print(item)
