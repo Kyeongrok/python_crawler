93cbfe29ec77f380953d7992adc42f3c6ecbcbde
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-03-17 15:35:14 +0900

    2개씩 다루기

--- ebay/allOrdersParser.py
+++ ebay/allOrdersParser.py
@@ -14,25 +14,22 @@ content = bsObj.find("table", {"id":"content"})
 mu_active_tbl_id = content.find("div", {"id":"mu_active_tbl_id"})
 tbodys = mu_active_tbl_id.findAll("tbody")
 
+infos = []
 for item in tbodys:
-
     if( len(item.attrs) != 0):
-        print(item.attrs['class'])
-        try:
-            buyerEmail = item.find("td", {"id":"BuyerEmail"})
+
+        if(len(item.attrs['class']) == 2):
             purchasedQty = item.find("td", {"id": "PurchasedQty"})
             qty = purchasedQty.find("div").text
-
+            infos.append(str(qty))
+        elif(len(item.attrs['class']) == 1):
+            buyerEmail = item.find("td", {"id": "BuyerEmail"})
             aTag = buyerEmail.find("a")
-            try:
-                productName = aTag['title']
-                print(productName)
-            except KeyError:
-                print(qty)
-                pass
+            infos.append(aTag['title'])
 
-        except AttributeError:
-            pass
+        if(len(infos) == 2):
+            print(infos)
+            infos = []
 
 
 
