06deb9e4d5fb50aa50123c241ccd13a77c50a5eb
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-03-17 12:54:42 +0900

    naver크롤링 추가

--- chapter1/1_2_2bs4_hello.py
+++ chapter1/1_2_2bs4_hello.py
@@ -1,6 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.google.com")
+html = urlopen("https://www.naver.com/")
 bsObj = BeautifulSoup(html.read(), "html.parser")
-print(bsObj)
+
+area_navigation = bsObj.find("div", {"class":"area_navigation"})
+an_l = area_navigation.find("ul", {"class":"an_l"})
+an_items = an_l.findAll("li")
+# print(an_items)
+
+for item in an_items:
+    an_txt = item.find("span", {"class":"an_txt"})
+    print(an_txt.text)
