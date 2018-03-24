1b64a8f21a2c1d47e3d091c73fccf1ccdcc70e91
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-03-24 12:55:51 +0900

    nate pann crawler

--- week3/nate_pann_crawler.py
+++ week3/nate_pann_crawler.py
@@ -1,5 +1,18 @@
 '''
 110 236 420658
 1. http://m.pann.nate.com/ 이 페이지 크롤링 하기
-
+2. data가 들어 있는 tag 찾기
 '''
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url_beauty = "http://m.pann.nate.com/talk/c20012"
+html = urlopen(url_beauty)
+bsObj = BeautifulSoup(html)
+
+lis = bsObj.find("ul", {"class":"list rank-list"}).findAll("li")
+
+for li in lis:
+    li_tit = li.find("span", {"class":"tit"})
+    print(li_tit.text)
