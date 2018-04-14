645c1f0e9f4cd3e4212f3ac7d12748184a23c544
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-04-14 13:08:15 +0900

    naver메뉴 뽑아내기

--- com/week2/01_naver_crawl.py
+++ com/week2/01_naver_crawl.py
@@ -7,15 +7,18 @@ html = urlopen(url)
 
 # ------- 모래 가지고 오기 ----
 
-bsObj = BeautifulSoup(html)
+bsObj = BeautifulSoup(html, "html.parser")
 atags = bsObj.findAll("a", {"class":"an_a mn_mail"})
 
 ul_an_l = bsObj.find("ul", {"class":"an_l"})
 lis = ul_an_l.findAll("li")
 
-for item in lis:
+for li in lis:
     # item에 find를 해서 a tag만 뽑아 보세요
-    print(item.find("a"))
+
+    # li에서 a를 찾아보세요.
+    atag = li.find("a")
+    print(atag)
 
 
 
---
+++ com/week2/02_list_array.py
@@ -0,0 +1,19 @@
+item = 1
+item2 = 2
+item3 = 3
+
+num_list = [item, item2, item3]
+
+print(num_list)
+
+extracted_item = num_list[0]
+
+print(extracted_item)
+
+# 두번째 item을 출력 하려면 어떻게 해야 할까요?
+# num_list의 두번째 item을 출력 해보세요.
+
+print(num_list[1])
+
+# 모두 출력을 하고 싶다
+# 하나씩 뽑아서 출력 하려면?
