b08d7dffec5eb5b4d87a73f1e3cf4e81186fa689
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-04-24 22:54:20 +0900

    naver crawl add

--- com/week2_crawl/02_naver_menu_crawl.py
+++ com/week2_crawl/02_naver_menu_crawl.py
@@ -1,11 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-# mac:ctrl + g, window:alt + j
 url = "https://www.naver.com/"
 html = urlopen(url)
 
-print(html.read())
 
 bsObj = BeautifulSoup(html, "html.parser")
 atags = bsObj.findAll("a", {"class":"an_a mn_mail"})
@@ -13,10 +11,10 @@ atags = bsObj.findAll("a", {"class":"an_a mn_mail"})
 ul_an_l = bsObj.find("ul", {"class":"an_l"})
 lis = ul_an_l.findAll("li")
 
-
 for li in lis:
-    # item에 find를 해서 a tag만 뽑아 보세요
+    try:
+        atag = li.find("a")
+        print(atag)
+    except:
+        print("", end="")
 
-    # li에서 a를 찾아보세요.
-    atag = li.find("a")
-    print(atag)
