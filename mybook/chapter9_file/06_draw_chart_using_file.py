7b7c10e5d978899f464a62750af051606cada0d3
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-06-03 12:28:59 +0900

    draw chart from data

--- mybook/chapter9_file/06_draw_chart_using_file.py
+++ mybook/chapter9_file/06_draw_chart_using_file.py
@@ -4,11 +4,7 @@ import matplotlib.pylab as plt
 f1 = open("./save_line.txt", mode='r')
 lines = f1.readlines()
 
-datas = []
-for line in lines:
-    datas.append(line.replace("\n", ""))
-
-print(datas)
+datas = [line.replace("\n", "") for line in lines]
 
 plt.plot(np.arange(1, len(datas)+1, 1), datas)
 plt.show()
