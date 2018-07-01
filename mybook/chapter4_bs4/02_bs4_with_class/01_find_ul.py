8bb40e79d7e1752065c289b5cd900d256e4b0867
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-07-01 12:15:49 +0900

    add desc

---
+++ mybook/chapter4_bs4/02_bs4_with_class/01_find_ul.py
@@ -0,0 +1,23 @@
+import bs4
+
+html_str = """
+<html>
+    <body>
+        <ul class="greet">
+            <li>hello</li>
+            <li>bye</li>
+            <li>welcome</li>
+        </ul>
+        <ul class="reply">
+            <li>ok</li>
+            <li>no</li>
+            <li>sure</li>
+        </ul>
+    </body>
+</html>
+"""
+
+bs_obj = bs4.BeautifulSoup(html_str, "html.parser")
+
+ul = bs_obj.find("ul")
+print(ul)
