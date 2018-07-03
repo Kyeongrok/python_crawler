58fa8bb036d9580aa07ba015f5efae02b45d2e80
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-07-03 21:21:07 +0900

    inisfree 주소 추가

--- test/01_inisfree.py
+++ test/01_inisfree.py
@@ -1,7 +1,6 @@
 import requests
 import bs4
 
-# 제품 정보 추출하기
 def get_product_info(item):
     p_name_obj = item.find("p", {"class":"pdtName"})
     product_name = p_name_obj.find("em").text
@@ -15,16 +14,19 @@ def get_product_info(item):
     link = p_name_obj.find("a")['href']
     return {"soldout":sold_out, "product_name":product_name, "price":price, "link":link}
 
-# 페이지에서 제품 정보 추출하기
 def get_page_product(url):
     url = url
     result = requests.get(url)
+
     bs_obj = bs4.BeautifulSoup(result.content, "html.parser")
+
     list_cons = bs_obj.findAll("div", {"class":"listCon"})
+
     return [get_product_info(item) for item in list_cons ]
 
+
 urls = [
-    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UA&catCd02=&tp=1&pageNo=1&sortStr=",
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UA&catCd02=&tp=1&pageNo=1&sortStr=", # UA = 스킨케어 10, UM = 5
     "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UA&catCd02=&tp=1&pageNo=2&sortStr=",
     "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UA&catCd02=&tp=1&pageNo=3&sortStr=",
     "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UA&catCd02=&tp=1&pageNo=4&sortStr=",
@@ -62,7 +64,20 @@ urls = [
     "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UN&catCd02=&tp=1&pageNo=10&sortStr=",
     "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UN&catCd02=&tp=1&pageNo=11&sortStr=",
 
-    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UD&catCd01=&tp=1&pageNo=1&sortStr=",
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UD&catCd02=&tp=1&pageNo=1&sortStr=", # UD 클렌징 5
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UD&catCd02=&tp=1&pageNo=2&sortStr=", # UD 클렌징 5
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UD&catCd02=&tp=1&pageNo=3&sortStr=", # UD 클렌징 5
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UD&catCd02=&tp=1&pageNo=4&sortStr=", # UD 클렌징 5
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UD&catCd02=&tp=1&pageNo=5&sortStr=", # UD 클렌징 5
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UD&catCd02=&tp=1&pageNo=6&sortStr=", # UD 클렌징 5 ctrl+d 한줄복사
+
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UF&catCd02=&tp=1&pageNo=1&sortStr=",
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UF&catCd02=&tp=1&pageNo=2&sortStr=",
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UF&catCd02=&tp=1&pageNo=3&sortStr=",
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UF&catCd02=&tp=1&pageNo=4&sortStr=",
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UF&catCd02=&tp=1&pageNo=5&sortStr=",
+    "http://www.innisfree.com/kr/ko/ProductListSub.do?catCd01=UF&catCd02=&tp=1&pageNo=6&sortStr=",
+
 
     ]
 
@@ -71,5 +86,3 @@ for url in urls:
     for product_info in items:
         result = "{}${}${}$http://www.innisfree.com{}".format(product_info['product_name'], product_info['price'], product_info['soldout'], product_info['link'])
         print(result)
-
-
