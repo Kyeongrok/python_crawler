d6261147e185fa380455d9af1c7ef165f1456e34
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-08-11 12:43:35 +0900

    add crawl my info

--- com/chapter12_selenium/02_my_info.py
+++ com/chapter12_selenium/02_my_info.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome("../../chrome/mac/chromedriver")
+driver = webdriver.Chrome("/Users/ydseo/chromedriver")
 driver.implicitly_wait(3)
 
 driver.get("https://nid.naver.com/nidlogin.login")
 
 login_id = driver.find_element_by_name("id")
 login_passwd = driver.find_element_by_name("pw")
-login_id.send_keys("world")
-login_passwd.send_keys("hello")
+login_id.send_keys("ceoori")
+login_passwd.send_keys("z1z1z1")
 
 login_button = driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input')
 login_button.click()
+
+driver.get("https://nid.naver.com/user2/help/myInfo.nhn?lang=ko_KR")
+
