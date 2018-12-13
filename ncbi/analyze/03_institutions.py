be8a5ec4033e4ac2d0a74f7b2c7373d979b8eb10
kyeongrok <oceanfog1@gmail.com>
2018-12-13 19:38:19 +0900

    nation replace하는 로직 추가

--- ncbi/analyze/03_institutions.py
+++ ncbi/analyze/03_institutions.py
@@ -1,22 +1,34 @@
 import pandas as pd
+import re
 
 location = "/Users/kyeongrok/Desktop/"
 urlPrefix = "https://www.ncbi.nlm.nih.gov/pubmed/"
-df = pd.read_json(location+"result1000.json")
+df = pd.read_json(location+"result3.json")
+
+regex = re.compile(r".\.*")
 
 def getNations(instutions):
     nations = []
     for institution in instutions:
         arr = institution.split(", ")
-        nations.append(arr[len(arr) - 1])
+        nation = arr[len(arr) - 1]
+
+        # China를 포함하고 있으면 China
+        if( nation.find("China") >= 0):
+            nation = "China"
+
+        if( nation.find("U.S.A") >= 0):
+            nation = "USA"
+
+        if( nation.find("U.K") >= 0):
+            nation = "UK"
+
+        nation = re.sub("\..*", "", nation)
+        nations.append(nation)
+    print(nations)
     return nations
 
 df['nation'] = df.apply(lambda row: getNations(row['institutions']), axis=1)
 
-# for index, row in df.iterrows():
-#     print(urlPrefix + str(row['id']), len(row['institutions']), row['institutions'])
-#     row['nation'] = "hh"
 
-print(df['nation'])
-
-
+df.to_json(location + "result4.json")
