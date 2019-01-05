5a1213c65334c58fe342c19d9cf5cb4c67c5d6eb
kyeongrok <oceanfog1@gmail.com>
2019-01-05 16:22:46 +0900

    add bs4

--- libs/coupang/productsParser2.py
+++ libs/coupang/productsParser2.py
@@ -1,6 +1,12 @@
+from bs4 import BeautifulSoup
+def getProduct(li):
+    print(li)
+    return {"name":"", "price":""}
 
 def getProducts(string):
-
-    # product list를 만든다.
+    bsObj = BeautifulSoup(string, "html.parser")
+    ul = bsObj.find("ul", {"id":"productList"})
+    lis = ul.findAll("li")
+    product = getProduct(lis[0])
 
     return []
