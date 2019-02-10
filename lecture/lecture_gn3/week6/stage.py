2e46cb6fc00b7aa97b2bc29dd6e91de0938da6fd
kyeongrok <oceanfog1@gmail.com>
2019-02-10 14:04:32 +0900

    stage

---
+++ lecture/lecture_gn3/week6/stage.py
@@ -0,0 +1,14 @@
+from libs.coupang.crawler import crawl
+from libs.coupang.parser import parse
+
+def getCategory(categoryNo):
+    result = []
+    for pageNo in range(1, 17 + 1):
+        pageString = crawl(categoryNo, pageNo)
+        products = parse(pageString)
+        result = result + products
+    return result
+
+result = getCategory("195297") # 1020
+print(len(result))
+
