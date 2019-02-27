3d07651f5e7ad4a4eb552c81bf83aed43d3f3a1a
Jihyun Jeong <jihyunjeong.me@gmail.com>
2019-02-27 17:20:38 +0900

    Update 01_jolse_mist.py
    403 Forbidden 에러가 발생 할 경우 User-Agent 로직 추가

--- com/chapter07_shoppingmal/01_shopping_mall/01_jolse_mist.py
+++ com/chapter07_shoppingmal/01_shopping_mall/01_jolse_mist.py
@@ -2,8 +2,13 @@ import requests
 import bs4
 
 url = "http://jolse.com/category/tonermist/43/"
-result = requests.get(url)
 
+# 403 Forbidden 에러가 발생 할 경우 아래 Headers를 추가 해 줍니다.
+# http://www.useragentstring.com/ 에 접속하여 "User-Agent" 에 추가 해줍니다.
+headers = {
+    "User-Agent": "*** 이곳에 해당하는 User-Agent를 추가합니다 (예시), Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) 26.109 Safari/537.36*******"
+}
+result = requests.get(url, headers=headers)
 bs_obj = bs4.BeautifulSoup(result.content, "html.parser")
 
 print(bs_obj)
