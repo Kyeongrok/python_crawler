b177a7c9d4aa59e5c6c91aef7495d6509987fda5
kyeongrok <oceanfog1@gmail.com>
2020-01-09 14:18:17 +0900

    pd_read, requests

--- sites/public_data/03_requests.py
+++ sites/public_data/03_requests.py
@@ -1,5 +1,5 @@
-import pandas as pd
+import requests
 
 path_code = "https://raw.githubusercontent.com/WooilJeong/PublicDataReader/f14e4de3410cc0f798a83ee5934070d651cbd67b/docs/%EB%B2%95%EC%A0%95%EB%8F%99%EC%BD%94%EB%93%9C%20%EC%A0%84%EC%B2%B4%EC%9E%90%EB%A3%8C.txt"
-code = pd.read_csv(path_code, encoding='cp949', sep='\t')
-print(code)
+data = requests.get(path_code)
+print(data.content)
