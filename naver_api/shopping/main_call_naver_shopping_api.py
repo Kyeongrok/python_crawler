8fb27dfde3f52efc9f6dc79e0018a1a4a10ea049
Kyeongrok <oceanfog1@gmail.com>
2020-08-24 15:22:09 +0900

    mod url

--- naver_api/shopping/main_call_naver_shopping_api.py
+++ naver_api/shopping/main_call_naver_shopping_api.py
@@ -2,7 +2,7 @@ import requests
 from urllib.parse import urlparse
 
 def call_naver_shopping_api(keyword, display, start):
-    url = "	https://openapi.naver.com/v1/search/shop.json?query=" + keyword + "&display=" + str(display) + "&start=" + str(start)
+    url = "https://openapi.naver.com/v1/search/shop.json?query=" + keyword + "&display=" + str(display) + "&start=" + str(start)
     result = requests.get(urlparse(url).geturl(),
               headers={"X-Naver-Client-Id":"PqQ1Z3ohPXR5U6B_Ol2W",
                        "X-Naver-Client-Secret":"rSaY31bKRi"})
