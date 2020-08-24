a4cb63e95d09768c5e4f5040e34e4c27b2231ee9
Kyeongrok <oceanfog1@gmail.com>
2020-08-24 15:16:15 +0900

    add naver shopping api crawl

---
+++ naver_api/shopping/main_call_naver_shopping_api.py
@@ -0,0 +1,12 @@
+import requests
+from urllib.parse import urlparse
+
+def call_naver_shopping_api(keyword, display, start):
+    url = "	https://openapi.naver.com/v1/search/shop.json?query=" + keyword + "&display=" + str(display) + "&start=" + str(start)
+    result = requests.get(urlparse(url).geturl(),
+              headers={"X-Naver-Client-Id":"PqQ1Z3ohPXR5U6B_Ol2W",
+                       "X-Naver-Client-Secret":"rSaY31bKRi"})
+    return result.json()
+
+print(call_naver_shopping_api('형광등', 100, 1))
+
